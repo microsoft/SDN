f454292ba6375162a1cb5d1f51f5f85880fa46bb
JMesser81 <jmesser@microsoft.com>
2016-06-07 10:11:06 -0700

    Added script to grab Port Profile Ids for all VMs

---
+++ Diagnostics/Get-AllPortProfiles.ps1
@@ -0,0 +1,17 @@
+﻿$PortProfileFeatureId = "9940cd46-8b06-43bb-b9d5-93d50381fd56"
+
+$vmNics = (Get-VMNetworkAdapter *)
+foreach ($vmNic in $vmNics) 
+{
+    write-output ("Getting port profile for VM NIC $($vmNic.Name) on VM $($vmNic.VMName)")
+    $currentProfile = Get-VMSwitchExtensionPortFeature -FeatureId $PortProfileFeatureId -VMNetworkAdapter $vmNic
+    if ( $currentProfile -eq $null) 
+    {
+        write-output "WARNING: $vmNic.Name does not have a port profile"
+    } else 
+    {
+        write-output ("Port Profile Id:   $($currentProfile.SettingData.ProfileId)")
+        write-output ("Port Profile Data: $($currentProfile.SettingData.ProfileData)")
+    }
+}
+    
