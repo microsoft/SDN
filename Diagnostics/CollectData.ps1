ecbba4436e67f6f6d0d5c1f7ee2d6b6733cdb335
JMesser81 <jmesser@microsoft.com>
2016-06-09 09:55:55 -0700

    Updated diagnostic tools and troubleshooting guidance

--- Diagnostics/CollectData.ps1
+++ Diagnostics/CollectData.ps1
@@ -1,12 +1,13 @@
 ﻿param(
-   [string][parameter(Mandatory=$true)]$NCRestFQDN = "sa18n30nc.sa18.nttest.microsoft.com",
-   [string[]][parameter(Mandatory=$true)]$NetworkControllerNodes = @("sa18n30nc1", "sa18n30nc2", "sa18n30nc3"),
-   [string][parameter(Mandatory=$true)]$OutputDirectory = "C:\SDNExpress\diagnostics",
+   [string][parameter(Mandatory=$true)]$NCRestFQDNorIP,
+   [string[]][parameter(Mandatory=$true)]$NetworkControllerNodes,
+   [string][parameter(Mandatory=$true)]$OutputDirectory,
    [string[]][parameter(Mandatory=$false)]$HyperVHostNodes,
-   [string][parameter(Mandatory=$true)]$Username = "SA18\jason",
-   [string][parameter(Mandatory=$true)]$Password = "!!123abc"
+   [string][parameter(Mandatory=$true)]$Username,
+   [string][parameter(Mandatory=$true)]$Password
 )
 
+$DIAGNOSTIC_LOGS_LOCATION = "C:\SDNDiagnostics\Logs\"
 
 #### Network Controller State
 
@@ -18,7 +19,16 @@ function DumpConfigurationState()
    )
 
    $outputfile = $OutputDirectory + "\\" + $nc.split(".")[0] + "_configurationstate.txt"
-   Debug-NetworkControllerConfigurationState -NcIpAddress $nc | Out-File $outputfile
+   if ($Global:AuthType -eq "X509")
+   {
+      Debug-NetworkControllerConfigurationState -NcIpAddress $nc | Out-File $outputfile
+   } 
+   elseif ($Global:AuthType -eq "Kerberos")
+   {
+      # Requires this to special file to be downloaded onto this host from GitHub
+      # https://github.com/Microsoft/SDN/tree/master/Diagnostics/https://github.com/Microsoft/SDN/blob/master/Diagnostics/Debug-NetworkControllerConfigurationVMM.ps1
+      Debug-NetworkControllerConfigurationStateVmm -NcIpAddress $nc | Out-File $outputfile
+   }
 }
 
 
@@ -55,12 +65,15 @@ function CollectNCLogs
    {
       # These files should all be on the NC node from which they were run, by convention, this is $ncnodes[0]
       $ncnode = $ncnodes[0]
-      robocopy /E /R:1 "\\$ncnode\C$\SDNDiagnostics\NetworkControllerState", $OutputDirectory, "*SlbConfigState.txt"      
-      robocopy /E /R:1 "\\$ncnode\C$\SDNDiagnostics\NetworkControllerState", $OutputDirectory, "*servicemodulereplicas.txt"      
+      robocopy /NP /NFL /NJS /NJH /NDL /E /R:1 "\\$ncnode\C$\SDNDiagnostics\NetworkControllerState", $OutputDirectory, "*SlbConfigState.txt"      
+      rm "\\$ncnode\C$\SDNDiagnostics\NetworkControllerState\*SlbConfigState.txt"
+      robocopy /NP /NFL /NJS /NJH /NDL /E /R:1 "\\$ncnode\C$\SDNDiagnostics\NetworkControllerState", $OutputDirectory, "*servicemodulereplicas.txt"
+      rm "\\$ncnode\C$\SDNDiagnostics\NetworkControllerState\*servicemodulereplicas.txt"
 
       foreach ($node in $ncnodes) 
       {
-         robocopy /E /R:1 "\\$node\C$\SDNDiagnostics\NetworkControllerState", $OutputDirectory, "*xml"
+         robocopy /NP /NFL /NJS /NJH /NDL /E /R:1 "\\$node\C$\SDNDiagnostics\NetworkControllerState", $OutputDirectory, "*xml"
+         rm "\\$node\C$\SDNDiagnostics\NetworkControllerState\*xml"
       }
    }
 }
@@ -75,8 +88,14 @@ function CollectHostLogs
 
    foreach ($node in $hostnodes) 
    {
-      robocopy /E /R:1 "\\$node\C$\SDNDiagnostics\Logs", $OutputDirectory, "*PA.txt"
-      robocopy /E /R:1 "\\$node\C$\SDNDiagnostics\Logs", $OutputDirectory, "*ovsdb_ms_vtep.txt"
+      robocopy /NP /NFL /NJS /NJH /NDL /E /R:1 "\\$node\C$\SDNDiagnostics\Logs", $OutputDirectory, "*PA.txt"
+      rm "\\$node\C$\SDNDiagnostics\Logs\*PA.txt"
+      robocopy /NP /NFL /NJS /NJH /NDL /E /R:1 "\\$node\C$\SDNDiagnostics\Logs", $OutputDirectory, "*ovsdb_ms_vtep.txt"
+      rm "\\$node\C$\SDNDiagnostics\Logs\*ovsdb_ms_vtep.txt"
+      robocopy /NP /NFL /NJS /NJH /NDL /E /R:1 "\\$node\C$\SDNDiagnostics\Logs", $OutputDirectory, "*_ipconfigall.txt"
+      rm "\\$node\C$\SDNDiagnostics\Logs\*_ipconfigall.txt"
+      robocopy /NP /NFL /NJS /NJH /NDL /E /R:1 "\\$node\C$\SDNDiagnostics\Logs", $OutputDirectory, "*_hostnetworking.txt"
+      rm "\\$node\C$\SDNDiagnostics\Logs\*_hostnetworking.txt"
    }
 }
 
@@ -84,39 +103,51 @@ function CollectHostLogs
 
 try {
 
-   $NCRestIP = ""
-
-   # TODO: Would be awesome to have dynamically loaded "diagnostic modules" to collect additional info - specified at run-time or statically in script
-
-   # TODO: Determine if we're using X.509 certs or Kerberos for authentication (check cert store my or root)
-   $Global:authtype = "X509"
-   
-   $Global:NCRest = ""
-   if ($Global:authtype -eq "X509")
-   {
-      $Global:NCRest = $NCRestFQDN
-   }
-   
-   # TODO: Check to see if Output Directory exists
-
    # Create creds (assume creds are same for NC Nodes and Hyper-V Hosts)
    $securePassword = ConvertTo-SecureString $Password -AsPlainText -Force -Verbose
    $credential = New-Object System.Management.Automation.PSCredential($Username, $securePassword)  -Verbose
-   
-   Write-Output "Getting Network Controller state (Configuration and IMOS) by invoking REST surface from this host..."
-   DumpConfigurationState -nc $Global:NCRest -outputdirectory $OutputDirectory
-   TriggerImosDump($Global:NCRest)
 
    # Create Remote PowerShell session for first NC Node
    $ncnode = $NetworkControllerNodes[0]
    $NC_PSSession = New-PSSession -ComputerName $ncnode -Credential $credential -EnableNetworkAccess -Verbose
+
+   # Determine if we're using X509 or Kerberos for authentication
+   $auth = Invoke-Command -Session $NC_PSSession -ScriptBlock { 
+      $auth = (Get-NetworkControllerCluster).ClusterAuthentication
+      $auth 
+   }   
+   $Global:AuthType = $auth.Value
+
+   # Check to see if a cert with this subject name is on the localhost
+   $cert = Get-ChildItem cert:\localmachine\root | where { $_.Subject -eq "CN=$NCRestFQDNorIP" }
+   if ($cert -eq $null)
+   {
+      Write-Output "ERROR: NC REST Certificate is not available on this host"
+      Exit 
+   }
+   else
+   {
+      $Global:NCRest = $NCRestFQDNorIP
+   }
+      
+   # Check to see if Output Directory exists
+   If (-Not (Test-Path $OutputDirectory))
+   {
+      Write-Output "Output directory does not exist. Creating..."
+      md $OutputDirectory
+   }
+      
+   Write-Output "Getting Network Controller Configuration State by invoking REST surface from this host"
+   DumpConfigurationState -nc $Global:NCRest -outputdirectory $OutputDirectory
    
-   Write-Output "Getting the SLB Configuration state..."
-   Invoke-Command -Session $NC_PSSession -ScriptBlock { Debug-SlbConfigState }
+   #Write-Output "Dumping IMOS state"
+   #TriggerImosDump($Global:NCRest)
+     
+   #Write-Output "Dumping the SLB Configuration state"
+   #Invoke-Command -Session $NC_PSSession -ScriptBlock { Debug-SlbConfigState }
 
    Write-Output "Getting the Network Controller Service Modules' Replica Status..."
-   $ncreplica_outputfile = "C:\\SDNDiagnostics\\NetworkControllerState\\" + $($Global:NCRest).split(".")[0] + "_servicemodulereplicas.txt"
-   Write-Output $ncreplica_outputfile
+   $ncreplica_outputfile = "C:\\SDNDiagnostics\\NetworkControllerState\\" + $($Global:NCRest).split(".")[0] + "_servicemodulereplicas.txt"   
    Invoke-Command -Session $NC_PSSession -ArgumentList $ncreplica_outputfile -ScriptBlock { 
       param(
          [string][parameter(Mandatory=$true)]$ncreplica_outputfile
@@ -124,13 +155,16 @@ try {
       Get-NetworkControllerReplica |Out-File $ncreplica_outputfile 
    }
 
+   Remove-PSSession $NC_PSSession 
+
    # Collect data from Network Controller
    CollectNCLogs($NetworkControllerNodes)
    
    # Grab Data from each host
    foreach ($node in $HyperVHostNodes)
-   {
-      Write-Output "Grabbingdata on $node"
+   {      
+      Write-Output "Grabbing data on $node"
+
       $Host_PSSession = New-PSSession -ComputerName $node -Credential $credential -EnableNetworkAccess -Verbose      
       Invoke-Command -Session $Host_PSSession -ArgumentList $OutputDirectory, $node -ScriptBlock { 
          param 
@@ -139,42 +173,69 @@ try {
             [string][parameter(Mandatory=$true)]$node,
             [bool][parameter(Mandatory=$false)]$IncludeTraces = $true
          )
-                
+    
+         $PortProfileFeatureId = "9940cd46-8b06-43bb-b9d5-93d50381fd56"            
+         $DIAGNOSTIC_LOGS_LOCATION = "C:\SDNDiagnostics\Logs\"
+
          $service=Get-Service NCHostAgent
          if ($service.Status -ne "Stopped") 
          { 
-            $msvtep_outfile = "C:\SDNDiagnostics\Logs\" + $node + "_ovsdb_ms_vtep.txt"
+            $msvtep_outfile = $DIAGNOSTIC_LOGS_LOCATION + $node + "_ovsdb_ms_vtep.txt"
             Write-Output "Dump MS_VTEP"
             ovsdb-client.exe dump tcp:127.0.0.1:6641 ms_vtep > "$msvtep_outfile"
-            #ovsdb-client.exe dump tcp:127.0.0.1:6641 ms_firewall"$OutputDirectory\ovsdb_ms_firewall.txt" 
+            
+            $msfirewall_outfile = $DIAGNOSTIC_LOGS_LOCATION + $node + "_ovsdb_ms_firewall.txt" 
+            Write-Output "Dump MS_FIREWALL"
+            ovsdb-client.exe dump tcp:127.0.0.1:6641 ms_firewall > $msfirewall_outfile 
+            
             #ovsdb-client.exe dump tcp:127.0.0.1:6641 ms_service_insertion > "$OutputDirectory\ovsdb_ms_service_insertion.txt"
          }
         
-         $pa_outfile = "C:\SDNDiagnostics\Logs\" + $node + "_PA.txt"
+         $pa_outfile = $DIAGNOSTIC_LOGS_LOCATION + $node + "_PA.txt"
+         Write-Output "Getting Provider Addresses"
          Get-ProviderAddress | Out-File $pa_outfile 
-         #ipconfig /allcompartments /all > "$OutputDirectory\" + $node + "_ipconfigall.txt"
-         #C:\Tools\showAllPolicy.ps1 ExternalPrivate > "$OutputDirectory\policy.txt"
-         #Get-NetAdapter > "$OutputDirectory\networkadapters.txt"
-         #Get-NetAdapter | fl * >> "$OutputDirectory\networkadapters.txt"
-         #Get-VMNetworkAdapter -All > "$OutputDirectory\vmnetworkadapters.txt"
-         #get-netipaddress -IncludeAllCompartments > "$OutputDirectory\netipaddress.txt"
-         #get-netroute -IncludeAllCompartments > "$OutputDirectory\netroute.txt"
-         #Get-VMNetworkAdapterVlan > "$OutputDirectory\vmnetworkadaptersvlan.txt"
-         #get-vm | Get-VMNetworkAdapter | fl * >> "$OutputDirectory\vmnetworkadapters.txt"
-         #Get-VMNetworkAdapterIsolation | fl * > "$OutputDirectory\vmnetworkadapterisolation.txt"
-         #Get-VMSwitch  > "$OutputDirectory\vmswitches.txt"
-         #Get-VMSwitch | fl * >> "$OutputDirectory\vmswitches.txt"
-         #get-vm | Get-vmnetworkadapter | Foreach { C:\Tools\DumpVmPort.ps1 -VMName $_.VMName -VMNetworkAdapterName $_.Name  -OutFile "$OutputDirectory\vmports.txt" }
-         #Get-VMNetworkAdapterRoutingDomainMapping -ManagementOS > "$OutputDirectory\VMNetworkAdapterRoutingDomainMapping.txt"
-         #Get-VMNetworkAdapterRoutingDomainMapping -ManagementOS | fl * >> "$OutputDirectory\VMNetworkAdapterRoutingDomainMapping.txt"
-         #vfpctrl /list-queue /switch ExternalPrivate >> "$OutputDirectory\vfpctrl-list-queue.txt"
-         #vfpctrl /get-qos-config /switch ExternalPrivate >> "$OutputDirectory\vfpctrl-get-qos-config.txt"
+
+         $ipconfig_outfile = $DIAGNOSTIC_LOGS_LOCATION + $node + "_ipconfigall.txt"
+         Write-Output "Getting ipconfigs"
+         ipconfig /allcompartments /all > $ipconfig_outfile 
          
-    }
+         $hostnetwork_outfile = $DIAGNOSTIC_LOGS_LOCATION + $node + "_hostnetworking.txt"
+         Write-Output "Getting SDN VMSwitch"
+         $vmswitch = Get-VMSwitch | where { ($_ |Get-VMSwitchExtension -Name "Windows Azure VFP Switch Extension").Enabled }                 
+         $vmswitch |fl |Out-File $hostnetwork_outfile
 
-    CollectHostLogs($HyperVHostNodes)
+         $pnics = $vmswitch.NetAdapterInterfaceDescriptions
+         foreach ($pnic in $pnics)
+         {
+            Write-Output "Getting Net Adapter info for $pnic"
+            Get-NetAdapter -InterfaceDescription $pnic |fl >> $hostnetwork_outfile        
+         }
 
+         Write-Output "Getting VM NICs"
+         $vmnics = Get-VMNetworkAdapter * |where { $_.SwitchName -eq "$($vmswitch.Name)" }
+         $vmnics >> $hostnetwork_outfile         
+         foreach ($vmNic in $vmNics) 
+         {
+             write-output ("Getting port profile for VM NIC $($vmNic.Name) on VM $($vmNic.VMName)")
+             $currentProfile = Get-VMSwitchExtensionPortFeature -FeatureId $PortProfileFeatureId -VMNetworkAdapter $vmNic
+             if ( $currentProfile -eq $null) 
+             {
+                 write-output "WARNING: $vmNic.Name does not have a port profile" |Out-File $hostnetwork_outfile -Append
+             } else 
+             {
+                 write-output ("$($vmNic.VMName) Port Profile Id:   $($currentProfile.SettingData.ProfileId)") |out-file $hostnetwork_outfile -Append
+                 write-output ("$($vmNic.VMName) Port Profile Data: $($currentProfile.SettingData.ProfileData)") |out-file $hostnetwork_outfile -Append
+             }
+             Write-output ("Getting isolation settings for VM NIC $($vmNic.Name) on VM $($vmNic.VMName)")
+             Get-VMNetworkAdapterVlan $vmnic |out-file $hostnetwork_outfile -Append
+             Get-VMNetworkAdapterIsolation $vmnic |out-file $hostnetwork_outfile -Append
+
+             # TODO: Get vfp port state, layers, and rules
+          }                                       
+       }
+       Remove-PSSession $Host_PSSession    
    }
+   CollectHostLogs($HyperVHostNodes)
 
 } catch  
 {
