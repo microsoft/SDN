30f507dafc5fede7a5e73583aa8c87e06cc5c00c
Jason Messer <jasonatwork@gmail.com>
2016-08-12 15:26:14 -0700

    Update ConfigureMCNP.ps1
    Updated comments

--- Containers/ConfigureMCNP.ps1
+++ Containers/ConfigureMCNP.ps1
@@ -1,4 +1,6 @@
-﻿REG ADD "HKLM\SYSTEM\CurrentControlSet\Services\NcHostAgent\Parameters\Plugins\Vnet\ProxiedServices\c79d8d8d-bbb4-42ea-8a8f-a492efc40a94" /v "ServerAddress" /t REG_SZ /d localhost /f
+# Run this script on the Hyper-V Host which is hosting Container Host (Tenant) VMs
+
+REG ADD "HKLM\SYSTEM\CurrentControlSet\Services\NcHostAgent\Parameters\Plugins\Vnet\ProxiedServices\c79d8d8d-bbb4-42ea-8a8f-a492efc40a94" /v "ServerAddress" /t REG_SZ /d localhost /f
 REG ADD "HKLM\SYSTEM\CurrentControlSet\Services\NcHostAgent\Parameters\Plugins\Vnet\ProxiedServices\c79d8d8d-bbb4-42ea-8a8f-a492efc40a94" /v "ServiceName" /t REG_SZ /d mds /f
 REG ADD "HKLM\SYSTEM\CurrentControlSet\Services\NcHostAgent\Parameters\Plugins\Vnet\ProxiedServices\c79d8d8d-bbb4-42ea-8a8f-a492efc40a94" /v "ServerPort" /t REG_DWORD /d 6642 /f
 REG ADD "HKLM\SYSTEM\CurrentControlSet\Services\NcHostAgent\Parameters\Plugins\Vnet\ProxiedServices\c79d8d8d-bbb4-42ea-8a8f-a492efc40a94" /v "ProxyListeningPort" /t REG_DWORD /d 6642 /f
