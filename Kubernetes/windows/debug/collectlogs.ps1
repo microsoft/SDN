30592b6508f4ac209a5a3f87b95ea9ef99e0c6e4
Madhan Raj Mookkandy <madhanm@corp.microsoft.com>
2018-03-01 22:49:58 -0800

    Fix Kubernetes debug script

--- Kubernetes/windows/debug/collectlogs.ps1
+++ Kubernetes/windows/debug/collectlogs.ps1
@@ -4,12 +4,18 @@ $Network = "L2Bridge"
 
 $ScriptPath = Split-Path $MyInvocation.MyCommand.Path
 
+$outDir = [io.Path]::Combine($ScriptPath, [io.Path]::GetRandomFileName())
+md $outDir
+pushd 
+cd $outDir
 Get-hnsnetwork | Convertto-json -Depth 20 > network.txt
 Get-hnsendpoint | Convertto-json -Depth 20 > endpoint.txt
 Get-hnspolicylist | Convertto-json -Depth 20 > policy.txt
 
 vfpctrl.exe /list-vmswitch-port > ports.txt
-$ScriptPath\dumpVfpPolicies.ps1 -switchName $Network > vfpOutput.txt
+powershell $ScriptPath\dumpVfpPolicies.ps1 -switchName $Network > vfpOutput.txt
 
 ipconfig /all > ip.txt
 route print > routes.txt
+popd
+Write-Host "Logs are available at $outDir"
