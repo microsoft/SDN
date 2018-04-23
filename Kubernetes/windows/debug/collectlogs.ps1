c5172d0832e0d9b7e9ba774b6f9ed47a836ae646
Madhan Raj Mookkandy <madhanm@corp.microsoft.com>
2018-04-23 16:20:39 -0700

    Modify debug scripts to download the dependent scripts

--- Kubernetes/windows/debug/collectlogs.ps1
+++ Kubernetes/windows/debug/collectlogs.ps1
@@ -2,9 +2,15 @@ Param(
     [parameter(Mandatory = $false)] [string] $Network = "L2Bridge"
 )
 
-$BaseDir = "c:\k"
-ipmo $BaseDir\hns.psm1
+$BaseDir = "c:\k\debug"
+md $BaseDir -ErrorAction Ignore
 
+Start-BitsTransfer  "https://raw.githubusercontent.com/Microsoft/SDN/master/Kubernetes/windows/debug/dumpVfpPolicies.ps1" -Destination $BaseDir\dumpVfpPolicies.ps1
+Start-BitsTransfer  "https://raw.githubusercontent.com/Microsoft/SDN/master/Kubernetes/windows/hns.psm1" -Destination $BaseDir\hns.psm1
+Start-BitsTransfer  "https://raw.githubusercontent.com/Microsoft/SDN/master/Kubernetes/windows/debug/startpacketcapture.cmd" -Destination $BaseDir\startpacketcapture.cmd
+Start-BitsTransfer  "https://raw.githubusercontent.com/Microsoft/SDN/master/Kubernetes/windows/debug/stoppacketcapture.cmd" -Destination $BaseDir\stoppacketcapture.cmd
+
+ipmo $BaseDir\hns.psm1
 
 $ScriptPath = Split-Path $MyInvocation.MyCommand.Path
 
@@ -17,7 +23,7 @@ Get-hnsendpoint | Convertto-json -Depth 20 > endpoint.txt
 Get-hnspolicylist | Convertto-json -Depth 20 > policy.txt
 
 vfpctrl.exe /list-vmswitch-port > ports.txt
-powershell $ScriptPath\dumpVfpPolicies.ps1 -switchName $Network > vfpOutput.txt
+powershell $BaseDir\dumpVfpPolicies.ps1 -switchName $Network > vfpOutput.txt
 
 ipconfig /allcompartments /all > ip.txt
 route print > routes.txt
