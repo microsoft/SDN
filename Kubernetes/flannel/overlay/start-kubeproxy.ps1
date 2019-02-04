084d9cbe5f2eb2eabf38929dbaf6099ca17870dd
David Schott <daschott@microsoft.com>
2019-02-04 14:46:07 -0800

    updated kube-proxy scripts

--- Kubernetes/flannel/overlay/start-kubeproxy.ps1
+++ Kubernetes/flannel/overlay/start-kubeproxy.ps1
@@ -1,7 +1,7 @@
 Param(
+    [parameter(Mandatory = $false)] $LogDir = "C:\k",
     $NetworkName = "vxlan0",
     $ManagementIP
-
 )
 $networkName = $NetworkName.ToLower()
 
@@ -13,4 +13,4 @@ If((Test-Path c:/k/sourceVip.json)) {
 $hostMac=(Get-NetAdapter -InterfaceAlias (Get-NetIPAddress -IPAddress $ManagementIP).InterfaceAlias).MacAddress
 ipmo c:\k\hns.psm1
 Get-HnsPolicyList | Remove-HnsPolicyList
-c:\k\kube-proxy.exe --v=4 --proxy-mode=kernelspace --hostname-override=$(hostname) --kubeconfig=c:\k\config --network-name=$networkName --source-vip=$sourceVip --enable-dsr=false
+c:\k\kube-proxy.exe --v=4 --proxy-mode=kernelspace --feature-gates="WinOverlay=true,WinDSR=false" --hostname-override=$(hostname) --kubeconfig=c:\k\config --network-name=$networkName --source-vip=$sourceVip --enable-dsr=false --log-dir=$LogDir --logtostderr=false
--- Kubernetes/windows/start-kubeproxy.ps1
+++ Kubernetes/windows/start-kubeproxy.ps1
@@ -1,9 +1,10 @@
 Param(
+    [parameter(Mandatory = $false)] $LogDir = "C:\k",    
     $NetworkName = "cbr0"
 )
 
-$env:KUBE_NETWORK=$NetworkName.ToLower()
+$networkName = $NetworkName.ToLower()
 ipmo c:\k\hns.psm1
 Get-HnsPolicyList | Remove-HnsPolicyList
 
-c:\k\kube-proxy.exe --v=4 --proxy-mode=kernelspace --hostname-override=$(hostname) --kubeconfig=c:\k\config
+c:\k\kube-proxy.exe --v=4 --proxy-mode=kernelspace --feature-gates="WinDSR=false" --hostname-override=$(hostname) --kubeconfig=c:\k\config --network=$networkName --enable-dsr=false --log-dir=$LogDir --logtostderr=false
