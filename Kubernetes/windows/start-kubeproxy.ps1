851ce5d547269f6dde6a1ee78c8988527f31ec12
David Schott <36676176+daschott@users.noreply.github.com>
2019-03-07 14:21:06 -0800

    enable kube-proxy to work for <v1.14 (#329)
    * Update start-kubeproxy.ps1
    
    * Update README.md

--- Kubernetes/windows/start-kubeproxy.ps1
+++ Kubernetes/windows/start-kubeproxy.ps1
@@ -7,4 +7,5 @@ $networkName = $NetworkName.ToLower()
 ipmo c:\k\hns.psm1
 Get-HnsPolicyList | Remove-HnsPolicyList
 
-c:\k\kube-proxy.exe --v=4 --proxy-mode=kernelspace --feature-gates="WinDSR=false" --hostname-override=$(hostname) --kubeconfig=c:\k\config --network=$networkName --enable-dsr=false --log-dir=$LogDir --logtostderr=false
+$env:KUBE_NETWORK=$networkName
+c:\k\kube-proxy.exe --v=4 --proxy-mode=kernelspace --hostname-override=$(hostname) --kubeconfig=c:\k\config --log-dir=$LogDir --logtostderr=false
