ef7ec1097bd60bed575368a788503c8eb2034c5d
sbangari <51677049+sbangari@users.noreply.github.com>
2022-11-10 14:27:33 -0800

    Script to capture Network Flows on Windows AKS nodes (#541)

---
+++ Kubernetes/windows/debug/captureNetworkFlows.ps1
@@ -0,0 +1,153 @@
+#Requires -RunAsAdministrator
+
+Param(
+    [parameter(Mandatory = $false)] [string] $NetworkName = "azure",
+    [parameter(Mandatory = $false)] [string] $ServiceEndpoint = "20.81.112.102:80",
+    [parameter(Mandatory = $false)] [string] $Verbosity = "Normal"
+)
+
+class Backend 
+{
+    [string] $IpAddress
+    [string] $EndPointId
+    [bool] $IsRemoteEndpoint
+    [string] $VfpPortGuid
+    [string] $InternalPort
+}
+
+class LoadBalancerPolicyData {
+    [string] $Id
+    [string] $NetworkId
+    [int] $State
+    [string] $Vip
+    [int] $ExternalPort
+    [int] $InternalPort
+    [int] $Protocol
+    [bool] $IsDsr
+    [bool] $LocalRoutedVip
+    [Backend[]] $Backends = @()
+}
+
+$networkId = (Get-HnsNetwork -Detailed | Where-Object Name -eq $networkName).Id
+$network = Get-HnsNetwork -Id $networkId -Detailed
+
+$hostVfpPort = ($network | ForEach-Object { $_.Layer.Resources.Allocators } | Where-Object Tag -eq "Host Vnic").EndpointPortGuid     
+$externalVfpPort = ($network | ForEach-Object { $_.Layer.Resources.Allocators } | Where-Object Tag -eq "External Adapter Info").ExternalNicPortGuid
+
+$endpointsToMonitor = @()
+$backendsToMonitor = @()
+$endpointsToMonitor += $ServiceEndpoint
+
+[LoadBalancerPolicyData] $loadBalancerPolicy = [LoadBalancerPolicyData]::new()
+$recomputePolicyData = $true
+while ($true) {
+
+    if ($recomputePolicyData) {
+
+        $endpointsToMonitor = @()
+        $backendsToMonitor = @()
+        $endpointsToMonitor += $ServiceEndpoint
+
+        $hnsPolicyList = Get-HnsPolicyList
+        foreach ($policy in $hnsPolicyList) {
+            $svcEndpoint = ""
+            if ($policy.Policies.LocalRoutedVip) {
+                $svcEndpoint += $network.ManagementIP
+            }
+            else {
+                $svcEndpoint += $policy.Policies.VIPs
+            }
+
+            $svcEndpoint += ":" 
+            $svcEndpoint += $policy.Policies.ExternalPort
+
+            if ($svcEndpoint -eq $ServiceEndpoint) {
+
+                $loadBalancerPolicy.Id = $policy.ID
+                $loadBalancerPolicy.IsDsr = $policy.Policies.IsDSR
+                $loadBalancerPolicy.ExternalPort = $policy.Policies.ExternalPort
+                $loadBalancerPolicy.InternalPort = $policy.Policies.InternalPort
+
+                if ($policy.Policies.LocalRoutedVip) {
+                    $loadBalancerPolicy.Vip = $network.ManagementIP
+                }
+                else {
+                    $loadBalancerPolicy.Vip = $policy.Policies.VIPs
+                }
+
+                foreach ($endpointReference in $policy.References) {
+                    $endpointReferenceGuid = $endpointReference.Replace("/endpoints/", "")
+
+                    $hnsEndpoint = Get-HnsEndpoint -Id $endpointReferenceGuid
+                    [Backend] $newBackEnd = [Backend]::new()
+                    $newBackEnd.IPAddress = $hnsEndpoint.IPAddress
+                    $newBackEnd.EndPointId = $hnsEndpoint.Id
+                    $newBackEnd.IsRemoteEndpoint = $hnsEndpoint.IsRemoteEndpoint -eq $true
+                    $newBackEnd.VfpPortGuid = ($hnsEndpoint.Resources.Allocators | Where-Object Tag -eq "Endpoint Port").EndpointPortGuid
+                    $newBackEnd.InternalPort = $policy.Policies.InternalPort
+                    $loadBalancerPolicy.Backends += $newBackEnd
+                }
+            }
+        }
+
+        foreach ($lbBackend in $loadBalancerPolicy.Backends) { 
+            $endpointsToMonitor += $lbBackend.IPAddress + ":" + $lbBackend.InternalPort
+            if ($lbBackend.IsRemoteEndpoint -ne $true) {
+                $backendsToMonitor += $lbBackend
+            }
+        }
+
+        $recomputePolicyData = $false
+    }
+
+    if ($loadBalancerPolicy.Id -eq "") {
+        Write-Host "HNS Load balancer policy for the provided service endpoint not available"
+        continue
+    }
+
+    $searchPatternForHostFlows = @()
+    $searchPatternForHostFlows += $ServiceEndpoint
+
+    if ($Verbosity -eq "Normal")
+    {
+        $hostVfpPortUnifiedFlows = vfpctrl.exe /port $hostVfpPort /sample-unified-flow "100000 0 1" | Out-String -Stream | Select-String -Pattern $searchPatternForHostFlows -Context 1, 4 
+        $externalVfpPortUnifiedFlows = vfpctrl.exe /port $externalVfpPort /sample-unified-flow "100000 0 1" | Out-String -Stream | Select-String -Pattern $searchPatternForHostFlows -Context 1, 4
+    }
+    elseif ($Verbosity -eq "Verbose") {
+        $hostVfpPortUnifiedFlows = vfpctrl.exe /port $hostVfpPort /sample-unified-flow "100000 0 1" | Out-String -Stream | Select-String -Pattern $endpointsToMonitor -Context 1, 4 
+        $externalVfpPortUnifiedFlows = vfpctrl.exe /port $externalVfpPort /sample-unified-flow "100000 0 1" | Out-String -Stream | Select-String -Pattern $endpointsToMonitor -Context 1, 4
+    }    
+        
+    foreach ($backendToMonitor in $backendsToMonitor) {
+        $containerPortUnifiedFlows = vfpctrl.exe /port $backendToMonitor.VfpPortGuid /sample-unified-flow "100000 0 1" | Out-String -Stream | Select-String -Pattern $endpointsToMonitor -Context 1, 4
+
+        foreach ($containerPortUnifiedFlow in $containerPortUnifiedFlows) {
+            $message = "{0}:{1} {2} {3} {4}" -f "POD VFP Port", $backendToMonitor.IpAddress, (Get-Date).ToString(), $(hostname), $containerPortUnifiedFlow.ToString().Trim()
+            Write-Host $message
+        }
+    }
+        
+    foreach ($externalUnifiedFlow in $externalVfpPortUnifiedFlows) {
+        $message = "{0} {1} {2} {3}" -f "External VFP Port", (Get-Date).ToString(), $(hostname), $externalUnifiedFlow.ToString().Trim()
+        Write-Host $message
+    }
+        
+    foreach ($hostUnifiedFlow in $hostVfpPortUnifiedFlows) {
+        $message = "{0} {1} {2} {3}" -f "Host VFP Port", (Get-Date).ToString(), $(hostname), $hostUnifiedFlow.ToString().Trim()
+        Write-Host $message
+    }
+
+    Start-Sleep 10
+    $lbPolicy = Get-HnsPolicyList | Where-Object Id -eq $loadBalancerPolicy.Id
+    if($null -eq $lbPolicy)
+    {
+        $recomputePolicyData = $true
+    }
+}
+
+
+
+
+
+
+
