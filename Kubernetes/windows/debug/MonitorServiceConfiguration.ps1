b350b0f3f62737bd8d9addf5e0b621d07622b0cf
sbangari <51677049+sbangari@users.noreply.github.com>
2022-11-24 00:10:14 -0800

    MonitorServiceConfiguration.ps1

---
+++ Kubernetes/windows/debug/MonitorServiceConfiguration.ps1
@@ -0,0 +1,63 @@
+#Requires -RunAsAdministrator
+
+Param(
+    [parameter(Mandatory = $false)] [string] $NetworkName = "azure",
+    [parameter(Mandatory = $false)] [string] $ServiceIP = "20.124.54.159",
+    [parameter(Mandatory = $false)] [string] $ServicePort = "5555"
+)
+
+
+class LoadBalancerPolicyData {
+    [string] $Id
+    [bool] $IsDsr
+    [bool] $IsVfpHairpinRulePlumbed
+}
+
+$networkId = (Get-HnsNetwork -Detailed | Where-Object Name -eq $networkName).Id
+$network = Get-HnsNetwork -Id $networkId -Detailed
+
+$hostVfpPort = ($network | ForEach-Object { $_.Layer.Resources.Allocators } | Where-Object Tag -eq "Host Vnic").EndpointPortGuid     
+
+
+[LoadBalancerPolicyData] $loadBalancerPolicy = [LoadBalancerPolicyData]::new()
+$loadBalancerPolicy.IsVfpHairpinRulePlumbed = $false
+
+while ($true) {
+
+    $hnsPolicyList = Get-HnsPolicyList
+    foreach ($policy in $hnsPolicyList) {
+        $vip = ""
+        if ($policy.Policies.LocalRoutedVip) {
+            $vip += $network.ManagementIP
+        }
+        else {
+            $vip += $policy.Policies.VIPs
+        }
+
+        if ($svcEndpoint -eq $ServiceIP -and $ServicePort -eq $policy.Policies.ExternalPort) {
+            $loadBalancerPolicy.Id = $policy.ID
+            $loadBalancerPolicy.IsDsr = $policy.Policies.IsDSR
+        }
+    }
+
+    if ($loadBalancerPolicy.Id -eq "") {
+        Write-Host (Get-Date).ToString() + "HNS Load balancer policy missing for the service"
+    }
+
+    $hairPinRulePattern = $ServiceIP + "_" + $ServicePort
+    $hairpinRuleCount = (vfpctrl.exe /port $hostVfpPort /layer SLB_HAIRPIN_LAYER /list-rule | Select-String -Pattern $hairPinRulePattern).Count
+    $loadBalancerPolicy.IsVfpHairpinRulePlumbed = ($hairpinRuleCount -gt 0)
+
+    if ($loadBalancerPolicy.IsVfpHairpinRulePlumbed -eq $false) {
+        Write-Host (Get-Date).ToString() + " Hairpin rule missing for the Service"
+    }
+
+    Start-Sleep 1
+}
+
+
+
+
+
+
+
