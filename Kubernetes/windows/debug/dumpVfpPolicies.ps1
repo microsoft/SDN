b6acaebd6c1f5012aa2cbd275577937f7cf003fa
jayanthaMicrosoft <125352008+jayanthaMicrosoft@users.noreply.github.com>
2023-04-09 19:50:17 +0530

    Using "get-rule-counter" in place of "list-rule"
    Replacing "/list-rule" with "/get-rule-counter" command. "/get-rule-counter" command lists the rules as well as respective counter values.

--- Kubernetes/windows/debug/dumpVfpPolicies.ps1
+++ Kubernetes/windows/debug/dumpVfpPolicies.ps1
@@ -34,10 +34,9 @@ foreach ($port in $ports) {
 	echo "Policy for port : " $portGuid  | Out-File $outfile -Encoding ascii -Append
 	& $vfpCtrlExe /list-space  /port $portGuid | Out-File $outfile -Encoding ascii -Append
 	& $vfpCtrlExe /list-mapping  /port $portGuid | Out-File $outfile -Encoding ascii -Append
-	& $vfpCtrlExe /list-rule  /port $portGuid | Out-File $outfile -Encoding ascii -Append
+	& $vfpCtrlExe /port $portGuid /get-rule-counter | Out-File $outfile -Encoding ascii -Append
 	& $vfpCtrlExe /port $portGuid /get-port-state | Out-File $outfile -Encoding ascii -Append
 	& $vfpCtrlExe /port $portGuid /list-nat-range | Out-File $outfile -Encoding ascii -Append
-	& $vfpCtrlExe /port $portGuid /get-rule-counter | Out-File $outfile -Encoding ascii -Append
 }
 
 & $vfpCtrlExe /switch $ports[0].'Switch Name'  /get-switch-forwarding-settings > vswitchForwarding.txt
